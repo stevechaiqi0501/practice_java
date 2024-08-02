0b1aaf32d463c650e0df68bd4f131f4bf58fd20a
shibasaki <berght3@gmail.com>
2024-08-02 20:01:31 +0800

    create practice_java

---
+++ javaFile/lesson2.java
@@ -0,0 +1,42 @@
+//クラス名を定義
+
+/*JAVAは自由に改行、空白をいれていい
+int,strで入力の囲い方が違う
+ */
+
+ class Sample4
+ {
+  public static void main(String[] args) 
+  {
+      System.out.println("円記号の表示");
+  }
+ }
+
+//practice 
+ class start
+{
+  // メソッド名を定義
+  public static void main(String[] args)
+  {
+    System.out.println('A');
+    System.out.print("javaをはじめましょう");
+    System.out.print("ようこそJAVAへ");
+    System.out.println("文字と数値を入力する");
+    System.out.println(123);
+    System.out.println("\\100找到了");
+    System.out.println("明天見");
+    System.out.println("1\t2\t3");
+    System.out.println("８進数:" + 06 + "\t" + 024 + "\t" + 015);
+    System.out.println("16進数:" + 0x6 + "\t" + 0x14 + "\t" + 0xD);
+
+/*文字型じゃなきゃよみこまない記述方法などがある
+例えば、intじゃないと8,16進数の記述方法は読めないし
+      strじゃなきゃタブはつかえない
+      また、ちがいリテラルの文法は＋でつなぐ
+ */
+
+    
+//printin printの違いは改行するかどうか
+  }
+}
+
