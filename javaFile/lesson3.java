f92d72a52af55275ba82e41ea4aeafa16d6c3853
shibasaki <berght3@gmail.com>
2024-08-02 20:13:37 +0800

    create practice_java

---
+++ javaFile/lesson3.java
@@ -0,0 +1,16 @@
+/*メモリについて
+ * 変数は基本アドレスを持った番地に保存される
+ * 型は宣言せなん
+ * 変数名は基本pythonと同じルール
+ */
+
+class lll
+ {
+    public static void main(String[] args) 
+    {
+        int num;
+        num = 3;
+        System.out.println("変数numの値は" + num + "です");
+        
+    }
+ }
